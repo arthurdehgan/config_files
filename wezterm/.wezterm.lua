e402d412bca502ede16edbcb61f2f2c2813c7575
arthurdehgan <arthurdehgan@hotmail.com>
2024-07-22 20:02:07 -0400

    update to wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,8 +9,8 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night"
 
-config.font = wezterm.font("Liga SFMono Nerd Font")
-config.font_size = 14
+config.font = wezterm.font("Liga SFMono Nerd Font", { weight = "Regular" })
+config.font_size = 12
 
 config.enable_tab_bar = false
 
@@ -18,12 +18,37 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 8
 config.keys = {
+	{ -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+		key = "LeftArrow",
+		mods = "CTRL",
+		action = wezterm.action({ SendString = "\x1bb" }),
+	},
+	{ -- Make Option-Right equivalent to Alt-f; forward-word
+		key = "RightArrow",
+		mods = "CTRL",
+		action = wezterm.action({ SendString = "\x1bf" }),
+	},
+	{
+		key = "q",
+		mods = "ALT",
+		action = wezterm.action.QuitApplication,
+	},
 	{
 		key = "w",
 		mods = "CMD",
 		action = wezterm.action.DisableDefaultAssignment,
 	},
 }
+config.window_padding = {
+	left = 5,
+	right = 5,
+	top = 0,
+	bottom = 0,
+}
+config.window_frame = {
+	border_bottom_height = "0",
+}
+config.window_close_confirmation = "AlwaysPrompt"
 
 -- and finally, return the configuration to wezterm
 return config
